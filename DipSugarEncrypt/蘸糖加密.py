4a24a6497e76af0ff661c71f1c98d89b844582e7
SmalBox <505414969@qq.com>
2023-03-30 00:13:52 +0800

    feat: 添加快速512方案

--- DipSugarEncrypt/蘸糖加密.py
+++ DipSugarEncrypt/蘸糖加密.py
@@ -18,25 +18,30 @@ with open(filePath, "rb") as infile, open(outFullPath, "wb") as outfile:
     data = bytearray(data)
 
     # 对文件进行异或加密
+    maxLength = 512
     for i in range(0, len(data), 4):
-        temp = []
-        temp.append(data[i])
-        if i + 1 < len(data):
-            temp.append(data[i + 1])
-        if i + 2 < len(data):
-            temp.append(data[i + 2])
-        if i + 3 < len(data):
-            temp.append(data[i + 3])
-        value = int.from_bytes(temp, "little")
-        encryptedValue = value ^ key
-        encrypted_data = encryptedValue.to_bytes(4, "little")
-        data[i] = encrypted_data[0]
-        if i + 1 < len(data):
-            data[i + 1] = encrypted_data[1]
-        if i + 2 < len(data):
-            data[i + 2] = encrypted_data[2]
-        if i + 3 < len(data):
-            data[i + 3] = encrypted_data[3]
+        if i < maxLength:
+            temp = []
+            temp.append(data[i])
+            if i + 1 < len(data):
+                temp.append(data[i + 1])
+            if i + 2 < len(data):
+                temp.append(data[i + 2])
+            if i + 3 < len(data):
+                temp.append(data[i + 3])
+            value = int.from_bytes(temp, "little")
+            encryptedValue = value ^ key
+            encrypted_data = encryptedValue.to_bytes(4, "little")
+            data[i] = encrypted_data[0]
+            if i + 1 < len(data):
+                data[i + 1] = encrypted_data[1]
+            if i + 2 < len(data):
+                data[i + 2] = encrypted_data[2]
+            if i + 3 < len(data):
+                data[i + 3] = encrypted_data[3]
+        else:
+            #data[i] = data[i] ^ 255
+            pass
 
     # 将加密后的数据写入保存文件
     outfile.write(data)
